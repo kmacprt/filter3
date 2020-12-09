fd6e28dc39fb4bc1bb49771f72fa73f8d99ab74b
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-09 05:54:01 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -57,7 +57,7 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    START_PHOTTO = os.environ.get("START_PHOTTO", 'https://telegra.ph/file/7703ef03a791f158e28db.jpg')
+    START_PHOTTO = os.environ.get("START_PHOTTO", 'https://telegra.ph/file/44706b57abb809b53c68d.png')
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
     OWNER_NAME = os.environ.get("OWNER_NAME", None)
 
