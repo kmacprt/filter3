512ca5c1e7dad202322df0a05d07c6ce7dc2a5cb
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-30 10:47:03 -0800

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -6,3 +6,6 @@ class Development(Config):
     API_KEY = "1423025003:AAFoeqQp167vO8zMvH4ZHsLzK7sbooXpVms"  # your api key, as provided by the @botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://kmac:dkkaj0123456@postgresql/postgres'  # sample db credentials
     START_PHOTTO = 'https://telegra.ph/file/44706b57abb809b53c68d.png'
+    WEBHOOK = False
+    MESSAGE_DUMP = None
+    STRICT_GBAN = False
