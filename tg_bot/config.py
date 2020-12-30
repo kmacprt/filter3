17ecc1da378da80cafac3d2b9475388aa004ec57
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-30 10:39:23 -0800

    Create config.py

---
+++ tg_bot/config.py
@@ -0,0 +1,8 @@
+from tg_bot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 1131653685  # your telegram ID
+    OWNER_USERNAME = "Kavinduaj"  # your telegram username
+    API_KEY = "1423025003:AAFoeqQp167vO8zMvH4ZHsLzK7sbooXpVms"  # your api key, as provided by the @botfather
+    SQLALCHEMY_DATABASE_URI = 'postgresql://kmac:dkkaj0123456@postgresql/postgres'  # sample db credentials
+    START_PHOTTO = 'https://telegra.ph/file/44706b57abb809b53c68d.png'
